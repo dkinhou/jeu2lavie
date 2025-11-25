6ec8f2a2235d3984ace9a927d9ab7be1a9709cb9
aefs <acachaelie@gmail.com>
2025-11-25 09:19:02 +0100

    cellules

--- cellules.hpp
+++ cellules.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
-class cellsule {
+class cellules {
   public:
     virtual bool estVivante() const = 0;
     virtual void setVivante(bool etat) = 0;
-    virtual ~cellsule() {}
+    virtual ~cellules() {}
 };
 
+class   celluleVivante : public cellules {
+  private:
+    bool vivante;
+
+  public:
+    celluleVivante() : vivante(true) {}
+    bool estVivante() const override { return vivante; }
+    void setVivante(bool etat) override { vivante = etat; }
+}; 
+
+class celluleMorte : public cellules {
+  private:
+    bool vivante;
+
+  public:
+    celluleMorte() : vivante(false) {}
+    bool estVivante() const override { return vivante; }
+    void setVivante(bool etat) override { vivante = etat; }
+};
